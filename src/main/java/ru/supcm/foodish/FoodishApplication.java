4d3fe0d79b7cf329d50d43dc233cded517f52fe4
supcm <supcm@mail.ru>
2025-03-29 21:53:16 +0800

    Init commit

---
+++ src/main/java/ru/supcm/foodish/FoodishApplication.java
@@ -0,0 +1,13 @@
+package ru.supcm.foodish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/supcm/foodish/FoodishApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.supcm.foodish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
